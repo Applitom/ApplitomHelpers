a212edf06ad4d3f57d03b32a4b9e4459b410a78b
Tomer Shalom <applitom@gmail.com>
2017-04-17 12:28:16 +0300

    Initial Commit

---
+++ ApplitomHelpers/ApplitomHelpers.h
@@ -0,0 +1,19 @@
+//
+//  ApplitomHelpers.h
+//  ApplitomHelpers
+//
+//  Created by Tomer Shalom on 17/04/2017.
+//  Copyright © 2017 Applitom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApplitomHelpers.
+FOUNDATION_EXPORT double ApplitomHelpersVersionNumber;
+
+//! Project version string for ApplitomHelpers.
+FOUNDATION_EXPORT const unsigned char ApplitomHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplitomHelpers/PublicHeader.h>
+
+
